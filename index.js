0862b243cab7f040b00b8d3ad53e9f9890db3528
Spencer Benson <bennspencer@gmail.com>
2022-02-03 03:25:13 +0300

    connect database

--- index.js
+++ index.js
@@ -5,29 +5,32 @@ const dotenv = require("dotenv")
 const helmet = require("helmet")
 const morgan = require("morgan")
 const port = 8800
+const userRoute = require("./routes/users")
+const authRoute = require("./routes/auth")
 
 dotenv.config()
 
-mongoose.connect(
-  process.env.MONGO_URL,
-  {
+mongoose
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  },
-  () => {
-    Console.log(`Connected to MongoDB`)
-  }
-)
+  })
+  .then(() => console.log("Database connected!"))
+  .catch((err) => console.log(err))
 
 // Middlewares
 app.use(express.json())
 app.use(morgan("common"))
 app.use(helmet())
 
+// Routes middleware
+app.use("/api/users", userRoute)
+app.use("/api/auth", authRoute)
+
 app.get("/", (req, res) => {
-  res.status("200").send("Welcome to the socialme homepage")
+  res.send("Welcome to the socialme homepage")
 })
 
 app.listen(port, () => {
-  console.log(`Backen server running on port ${port}`)
+  console.log(`Backend server running on port ${port}`)
 })
